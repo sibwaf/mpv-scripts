e4fb662207e024f26d7763c9551e429898267974
sibwaf <7131069+sibwaf@users.noreply.github.com>
2024-08-01 00:43:42 +0400

    [reload] fix compatibility with mpv 0.38+ #16

--- reload.lua
+++ reload.lua
@@ -1,18 +1,24 @@
 --[[
-	reload / by sibwaf / https://github.com/sibwaf/mpv-scripts
+    reload / by sibwaf / https://github.com/sibwaf/mpv-scripts
 
-	Reopens the current playing file and seeks to the same timestamp on a button press ([Shift+R] by default).
+    Reopens the current playing file and seeks to the same timestamp on a button press ([Shift+R] by default).
     Useful for situations when you are watching YouTube/streams and your connection breaks for some reason.
 
-	MIT license - do whatever you want, but I'm not responsible for any possible problems.
-	Please keep the URL to the original repository. Thanks!
+    MIT license - do whatever you want, but I'm not responsible for any possible problems.
+    Please keep the URL to the original repository. Thanks!
 ]]
 
-function reload()
+local mp = require("mp")
+
+local function reload()
     local path = mp.get_property("path")
     if path ~= nil then
-        local time = mp.get_property_number("time-pos")
-        mp.commandv("loadfile", path, "replace", "start=" .. time)
+        local args = {}
+        args["name"] = "loadfile"
+        args["url"] = path
+        args["flags"] = "replace"
+        args["options"] = "start=" .. mp.get_property_number("time-pos")
+        mp.command_native(args)
     end
 end
 
